0a16e53da3ce90b98c69e8dc413211bc6ca68282
SimonKaluza <kaluza.simon@gmail.com>
2014-08-06 18:48:06 -0400

    Update rails.rb
    Commented out the gem_package installation of rake as it's included by default in Ruby versions > 2 (and thus conflicts)

--- providers/rails.rb
+++ providers/rails.rb
@@ -115,12 +115,12 @@ action :before_migrate do
   end
 
   gem_names = new_resource.gems.map { |gem, ver| gem }
-  if new_resource.migration_command.include?('rake') && !gem_names.include?('rake')
-    gem_package "rake" do
-      action :install
-      not_if "which rake"
-    end
-  end
+  #if new_resource.migration_command.include?('rake') && !gem_names.include?('rake')
+  #  gem_package "rake" do
+  #    action :install
+  #    not_if "which rake"
+  #  end
+  #end
 
 end
 
